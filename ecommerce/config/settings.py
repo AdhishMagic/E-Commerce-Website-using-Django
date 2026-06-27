83d9b2ca4ae17465e15dac684b84db4a04d67745
AdhishMagic <balaadhish.cbe@gmail.com>
2026-06-27 23:23:53 +0530

    Initial folderr structure are applied

--- ecommerce/config/settings.py
+++ ecommerce/config/settings.py
@@ -41,6 +41,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "accounts",
+    "products",
+    "cart",
+    "wishlist",
+    "orders",
+    "payments",
+    "reviews",
+    "coupons",
+    "dashboard",
+
 ]
 
 MIDDLEWARE = [
@@ -122,4 +132,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
